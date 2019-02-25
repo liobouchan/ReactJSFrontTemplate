c32996783d66ab8261c75d0363b83994bc0f8f8b
Leonardo <liogothicvampire@gmail.com>
2019-02-25 16:33:41 -0600

    Creating a Component unction insted of class

--- tic-tac-toe-game/src/index.js
+++ tic-tac-toe-game/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+/* class Square extends React.Component {
     render() {
         return (
             <button className="square" 
@@ -12,6 +12,14 @@ class Square extends React.Component {
             </button>
         );
     }
+} */
+
+function Square(props){
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
