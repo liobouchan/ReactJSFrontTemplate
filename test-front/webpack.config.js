00c4e4667afab75c5bf4d4b9144ab1ee9ae5b30e
Leonardo <liogothicvampire@gmail.com>
2019-02-26 17:46:11 -0600

    Adding the webpack configuration

---
+++ test-front/webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    entry: '${SRC_DIR}/index.jsx',
+    output: {
+      path: DIST_DIR,
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json', '.css']
+    },
+    module : {
+      rules : [
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+        },
+        {
+          test: /\.png$/,
+          loader: 'url-loader?limit=100000&minetype=image/png'
+        },
+        {
+          test: /\.jpg/,
+          loader: 'file-loader'
+        },
+        {
+          test : /\.jsx?/,
+          include : SRC_DIR,
+          loader : 'babel-loader',      
+          query: {
+            presets: ['react', 'es2017']
+         }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })
+    ]
+  };
